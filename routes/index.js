9c48e6313763158a567aff0a59c011b2b876b7df
ksut3955 <ksut3955@sysco.com>
2019-06-07 18:21:47 +0530

    A

--- routes/index.js
+++ routes/index.js
@@ -151,14 +151,14 @@ router.post('/reviews', async (req, res, next) => {
                     // const response = { conmpanyRating: conmpanyRating/overallCount, overallCount, productDetails}
 
                     let htmlDATA = `<h1> <b>Company Name </b> : ${companyName} </h1> 
-                        <h2><b> Company Ratings</b> : ${conmpanyRating/overallCount} </h2> `
+                        <h2><b> Company Ratings</b> : ${Math.round(conmpanyRating/overallCount,2)} </h2> `
                     
                     let productsInfo = ''
                     console.log(productDetails)
                     productDetails.forEach(product => {
                       let productData = `<h3> <b>Product Name</b> : ${product.name} </h3> 
                     <ul> 
-                      <li> <b>Rating</b> : ${product.rating}</li>
+                      <li> <b>Rating</b> : ${Math.round(product.rating,2)}</li>
                       <li> <b>Hot selling count</b> : ${product.counts} </li>
                       <li> <b>Ingredients</b> </li>
                       <ul> 
